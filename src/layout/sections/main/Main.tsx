e8bdfabfb3d3c1529255dbbb7d4aafaa2e8eab05
4iklins <4iklins@gmail.com>
2023-07-30 19:17:44 +0300

    main section styles

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,32 +1,85 @@
 import styled from 'styled-components';
 import { FlexContainer } from '../../../components/FlexContainer';
 import myPhoto from '../../../assets/img/photo.jpg';
+import { Container } from '../../../components/Container';
 
 export const Main = () => {
   return (
     <StyledMain>
-      <FlexContainer
-        align='center'
-        content='space-around'>
-        <div>
-          <span>Hi there</span>
-          <h2>I am Andrei Bortnik</h2>
-          <h1>A Web Developer. </h1>
-        </div>
-        <StyledPhoto src={myPhoto} />
-      </FlexContainer>
+      <Container>
+        <FlexContainer
+          align='center'
+          content='space-between'>
+          <div>
+            <SmallText>Hi there</SmallText>
+            <MainTitle>
+              I am <span>Andrei Bortnik</span>
+            </MainTitle>
+            <PageTitle>A Web Developer.</PageTitle>
+          </div>
+          <PhotoWrapper>
+            <StyledPhoto src={myPhoto} />
+          </PhotoWrapper>
+        </FlexContainer>
+      </Container>
     </StyledMain>
   );
 };
 
 const StyledMain = styled.section`
+  display: flex;
   min-height: 100vh;
-  background-color: #deffff;
 `;
 const StyledPhoto = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+`;
+
+const SmallText = styled.span`
+  font-size: 14px;
+  font-weight: 400;
+`;
+
+const MainTitle = styled.h2`
+  color: #fff;
+  font-family: Josefin Sans;
+  font-size: 50px;
+  font-weight: 700;
+  letter-spacing: 2.5px;
+  span {
+    position: relative;
+    z-index: 0;
+    &::before {
+      content: '';
+      position: absolute;
+      display: inline-block;
+      height: 20px;
+      width: 100%;
+      background-color: ${({ theme }) => theme.colors.accent};
+      z-index: -1;
+      bottom: 0;
+    }
+  }
+`;
+const PageTitle = styled.h1`
+  font-size: 27px;
+  font-weight: 400;
+`;
+
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+  &::before {
+    content: '';
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${({ theme }) => theme.colors.accent};
+    z-index: -1;
+  }
 `;
 
 export default Main;
