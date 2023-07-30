8e8a3cde530dd7bcef0a49388439f4ad8788ba7a
4iklins <4iklins@gmail.com>
2023-07-30 19:17:25 +0300

    header styles

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,22 +1,34 @@
 import styled from 'styled-components';
-import Menu from '../../components/menu/Menu';
 import Logo from '../../components/logo/Logo';
+import { Container } from '../../components/Container';
+import { FlexContainer } from '../../components/FlexContainer';
+import HeaderMenu from './headerMenu/HeaderMenu';
 
 const menu = ['Home', 'Skills', 'Works', 'Testimony', 'Contact'];
 
 export const Header = () => {
   return (
     <StyledHeader>
-      <Logo />
-      <Menu menu={menu} />
+      <Container>
+        <FlexContainer
+          content='space-between'
+          align='center'>
+          <Logo />
+          <HeaderMenu menu={menu} />
+        </FlexContainer>
+      </Container>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.header`
-  display: flex;
-  justify-content: space-between;
-  background-color: lightgreen;
+  background-color: rgb(31, 31, 32, 0.9);
+  position: fixed;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10000;
 `;
 
 export default Header;
---
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+
+const HeaderMenu = ({ menu }: { menu: string[] }) => {
+  return (
+    <StyledHeaderMenu>
+      <ul>
+        {menu.map(item => {
+          return (
+            <ListItem>
+              <Link href='#'>
+                {item}
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+                <Mask>
+                  <span>{item}</span>
+                </Mask>
+              </Link>
+            </ListItem>
+          );
+        })}
+      </ul>
+    </StyledHeaderMenu>
+  );
+};
+const StyledHeaderMenu = styled.nav`
+  ul {
+    display: flex;
+    gap: 30px;
+  }
+`;
+
+const Link = styled.a`
+  position: relative;
+  font-family: Josefin Sans;
+  font-size: 30px;
+  font-weight: 400;
+  color: transparent;
+`;
+
+const Mask = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 50%;
+  overflow: hidden;
+  color: ${({ theme }) => theme.colors.accent};
+  & + & {
+    top: 50%;
+    span {
+      display: inline-block;
+      transform: translateY(-50%);
+    }
+  }
+`;
+
+const ListItem = styled.li`
+  position: relative;
+  &::before {
+    content: '';
+    position: absolute;
+    display: inline-block;
+    height: 3px;
+    background-color: ${({ theme }) => theme.colors.accent};
+    top: 50%;
+    transform: translateY(-50%) scale(0);
+    left: -10px;
+    right: -10px;
+    z-index: 1;
+  }
+  &:hover {
+    &::before {
+      transform: translateY(-50%) scale(1);
+    }
+    ${Mask} {
+      transform: skewX(12deg) translateX(3px);
+      color: ${({ theme }) => theme.colors.font};
+      & + ${Mask} {
+        transform: skewX(12deg) translateX(-3px);
+      }
+    }
+  }
+`;
+export default HeaderMenu;
