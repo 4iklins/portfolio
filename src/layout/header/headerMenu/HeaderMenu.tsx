39d8126ed421a9521cbde62bfee2d0331b878c59
4iklins <4iklins@gmail.com>
2023-08-11 00:41:17 +0300

    burger menu state

--- src/layout/header/headerMenu/HeaderMenu.tsx
+++ src/layout/header/headerMenu/HeaderMenu.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import { S } from './HeaderMenu_Styles';
 
 const HeaderMenu = ({ menu }: { menu: string[] }) => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <S.HeaderMenu isOpen={false}>
-      <S.BurgerButton isOpen={false}>
+    <S.HeaderMenu isOpen={showMenu}>
+      <S.BurgerButton
+        isOpen={showMenu}
+        onClick={handleClick}>
         <span></span>
       </S.BurgerButton>
       <ul>
